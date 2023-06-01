feeb3376e4d95d198c6ad0210bb1da914a8b2fcf
heshan_sandeepa <heshan.sandeepa@freshmilksoftware.com>
2023-06-01 12:31:10 +0530

    proceed

--- UnScramble/app/src/main/java/com/example/unscramble/ui/GameUiState.kt
+++ UnScramble/app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -1,4 +1,7 @@
 package com.example.unscramble.ui
 
-data class GameUiState(val currentScrambleWord: String = "") {
-}
+data class GameUiState(val currentScrambleWord: String = "",
+                       val currentWordCount: Int = 1,
+                       val isGuessedWordWrong: Boolean = false,
+                       val score: Int = 0,
+                       val isGameOver: Boolean = false)
