7b18f2f7f69e97106f2d23e0d474f64dded2537f
Heshan <heshan@Heshans-Mac-mini.local>
2023-07-25 10:29:51 +0530

    pathway

--- LunchTray/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ LunchTray/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -16,45 +16,101 @@
 package com.example.lunchtray
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.lunchtray.datasource.DataSource
 import com.example.lunchtray.ui.OrderViewModel
+import com.example.lunchtray.ui.StartOrderScreen
 
 // TODO: Screen enum
 enum class LunchTrayScreen(@StringRes val title: Int) {
     Start(title = R.string.app_name),
-    Flavor(title = R.string.choose_entree),
-    Pickup(title = R.string.choose_side_dish),
+    EntreeMenu(title = R.string.choose_entree),
     SideDish(title = R.string.choose_side_dish),
-    Summary(title = R.string.order_summary)
+    Accompaniment(title = R.string.choose_accompaniment),
+    Checkout(title = R.string.order_checkout)
 }
 
-Start
-Entree menu
-Side dish menu
-Accompaniment menu
-Checkout
-
-
 // TODO: AppBar
+@Composable
+fun LunchTrayAppAppBar(
+    currentScreen: LunchTrayScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(stringResource(currentScreen.title)) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
+
 
 @Composable
-fun LunchTrayApp() {
+fun LunchTrayApp(
+    navController: NavHostController = rememberNavController()
+) {
     // TODO: Create Controller and initialization
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = LunchTrayScreen.valueOf(
+        backStackEntry?.destination?.route ?: LunchTrayScreen.Start.name
+    )
 
     // Create ViewModel
     val viewModel: OrderViewModel = viewModel()
 
     Scaffold(
         topBar = {
-            // TODO: AppBar
+            LunchTrayAppAppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp()  }
+            )
         }
     ) { innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
-
-        // TODO: Navigation host
+        NavHost(
+            navController = navController,
+            startDestination = LunchTrayScreen.Start.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = LunchTrayScreen.Start.name) {
+                StartOrderScreen(onStartOrderButtonClicked = { navController.navigate(LunchTrayScreen.EntreeMenu.name) })
+            }
+        }
     }
 }
